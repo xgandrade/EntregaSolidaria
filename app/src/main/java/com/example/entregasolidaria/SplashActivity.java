f7f7c7c8599dab5d921136be53664b00948eade5
Guilherme Andrade <guilherme_gsa@live.com>
2020-05-24 14:36:44 -0300

    RF01 Alterando tela de login e incluido tela Splash

--- app/src/main/java/com/example/entregasolidaria/SplashActivity.java
+++ app/src/main/java/com/example/entregasolidaria/SplashActivity.java
@@ -1,13 +1,27 @@
 package com.example.entregasolidaria;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER, WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), MainActivity.class));
+                finish();
+            }
+        }, 3000);
     }
 }
