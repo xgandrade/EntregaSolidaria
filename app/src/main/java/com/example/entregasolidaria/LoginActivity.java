712bd8bd0fa414de1af97dfdb754f88eac32e77d
Guilherme Andrade <guilherme_gsa@live.com>
2020-05-31 21:18:58 -0300

    Alterado metodo do LoginActivity

--- app/src/main/java/com/example/entregasolidaria/LoginActivity.java
+++ app/src/main/java/com/example/entregasolidaria/LoginActivity.java
@@ -11,7 +11,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_login);
     }
 
     public void BackClick(View view){
