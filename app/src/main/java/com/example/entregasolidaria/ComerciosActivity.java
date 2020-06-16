0fb0a78a39a65560a44f4bdb9c96ff6ee3b42928
Guilherme Andrade <guilherme_gsa@live.com>
2020-06-15 21:37:57 -0300

    Realizado integração entre as telas para navegação inicial

--- app/src/main/java/com/example/entregasolidaria/ComerciosActivity.java
+++ app/src/main/java/com/example/entregasolidaria/ComerciosActivity.java
@@ -1,6 +1,9 @@
 package com.example.entregasolidaria;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ComerciosActivity extends AppCompatActivity {
@@ -9,6 +12,11 @@ public class ComerciosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar_comercios);
+    }
+
+    public void BackComClick(View view){
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/example/entregasolidaria/LoginActivity.java
+++ app/src/main/java/com/example/entregasolidaria/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.entregasolidaria;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,11 +21,17 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
+    public void LoginClick(View view){
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+
+        Toast.makeText(this, R.string.LoginOK, Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
     public void BackClick(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
-
 
 }
