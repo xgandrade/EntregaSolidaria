4928330c4df545a5091e9d788e3d4cf0408a7cfd
José Victor <jvictorfrance@outlook.com>
2020-06-16 21:55:30 -0300

    Adicionando botão voltar na lista pedidos

--- app/src/main/java/com/example/entregasolidaria/ProdutosActivity.java
+++ app/src/main/java/com/example/entregasolidaria/ProdutosActivity.java
@@ -1,7 +1,9 @@
 package com.example.entregasolidaria;
 
-import android.database.sqlite.SQLiteDatabase;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -9,7 +11,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.entregasolidaria.Adapters.RecyclerProdutoAdapter;
 import com.example.entregasolidaria.Model.Produto;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -36,19 +37,22 @@ public class ProdutosActivity extends AppCompatActivity {
     }
 
     private void iniciaListaProdutos() {
+        produtos.add(new Produto(1,1,12.50,"Kit Kat",500));
+        produtos.add(new Produto(2,1,55.50,"Kit Kat Branco",500));
+        produtos.add(new Produto(3,1,33.50,"Kit Kat Preto",500));
+        produtos.add(new Produto(4,1,80.50,"Kit Kat Verde",500));
+        produtos.add(new Produto(1,1,12.50,"Kit Kat",500));
+        produtos.add(new Produto(2,1,55.50,"Kit Kat Branco",500));
+        produtos.add(new Produto(3,1,33.50,"Kit Kat Preto",500));
+        produtos.add(new Produto(4,1,80.50,"Kit Kat Verde",500));
+        produtos.add(new Produto(1,1,12.50,"Kit Kat",500));
+        produtos.add(new Produto(2,1,55.50,"Kit Kat Branco",500));
+        produtos.add(new Produto(3,1,33.50,"Kit Kat Preto",500));
+        produtos.add(new Produto(4,1,80.50,"Kit Kat Verde",500));
+    }
 
-        produtos.add(new Produto(1,1,12.50,"Kit Kat",500));
-        produtos.add(new Produto(2,1,55.50,"Kit Kat Branco",500));
-        produtos.add(new Produto(3,1,33.50,"Kit Kat Preto",500));
-        produtos.add(new Produto(4,1,80.50,"Kit Kat Verde",500));
-        produtos.add(new Produto(1,1,12.50,"Kit Kat",500));
-        produtos.add(new Produto(2,1,55.50,"Kit Kat Branco",500));
-        produtos.add(new Produto(3,1,33.50,"Kit Kat Preto",500));
-        produtos.add(new Produto(4,1,80.50,"Kit Kat Verde",500));
-        produtos.add(new Produto(1,1,12.50,"Kit Kat",500));
-        produtos.add(new Produto(2,1,55.50,"Kit Kat Branco",500));
-        produtos.add(new Produto(3,1,33.50,"Kit Kat Preto",500));
-        produtos.add(new Produto(4,1,80.50,"Kit Kat Verde",500));
-
+    public void VoltarClick(View view){
+        Intent intent = new Intent(this, ComerciosActivity.class);
+        startActivity(intent);
     }
 }
